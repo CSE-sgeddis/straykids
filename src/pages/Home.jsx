217a04088ee794d68b16efe731a6fdc75f5d7121
CSE-sgeddis <shalindixon@gmail.com>
2025-10-24 20:45:31 -0400

    reached stopping point..need to fix errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,17 @@ import "./../css/Home.css";
 
 const Home = () => {
     return(
-        <div id="main-page">
+        <div id="main-page"
+            style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL}/images/Stray-Kids-Rome-Roma-min-2-scaled.jpg)`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat'
+        }}
+        >
             <header>
                 <Link to="/" className="logo">
-                    <img src="/images/stay logo.jpg" width="142" height="91" alt="Website logo" />
+                    <img src={`${process.env.PUBLIC_URL}/images/stay logo.jpg`} width="142" height="91" alt="Website logo" />
                 </Link>
                 <div className="tagline">STAY CONNECTED - You Make Stray Kid STAY</div>
             </header>
