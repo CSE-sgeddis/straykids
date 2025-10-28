79df9d182b223a5b0af65f9d2b8aa0185895aed9
CSE-sgeddis <shalindixon@gmail.com>
2025-10-27 20:57:05 -0400

    ready to deploy

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Community from './pages/Community';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route>
           <Route index element={<Home />}/>
